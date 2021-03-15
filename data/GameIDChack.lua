8733e1783a1f9bb5ac74b1281b673e6537d37a76
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-03-15 21:51:21 +0300

    Update GameIDChack.lua

--- data/GameIDChack.lua
+++ data/GameIDChack.lua
@@ -2,5 +2,7 @@ if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Hysonax/HyHUB/main/data/Game%20Scripts/Arsenal/ArsenalMenu.lua",true))()
 elseif game.PlaceId == 6305942109 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Hysonax/HyHUB/main/data/Game%20Scripts/Laundry%20Simulator/LaundrySimulatorMenu.lua",true))()
+elseif game.PlaceId == 698448212 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/notclosure/new-years/main/happ.lua"))()
 else warn("Unknown game!!!")
 end
