fbe4abf3a8fa0edaba0e283a167f160eb8c35eea
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-03-16 20:07:43 +0300

    Update GameIDChack.lua

--- data/GameIDChack.lua
+++ data/GameIDChack.lua
@@ -1,7 +1,5 @@
 if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/iliasaw/SplashHub/main/data/Game%20Scripts/Arsenal/ArsenalMenu.lua",true))()
-elseif game.PlaceId == 6305942109 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hysonax/HyHUB/main/data/Game%20Scripts/Laundry%20Simulator/LaundrySimulatorMenu.lua",true))()
 elseif game.PlaceId == 698448212 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/notclosure/new-years/main/happ.lua"))()
 elseif game.PlaceId == 606849621 then
