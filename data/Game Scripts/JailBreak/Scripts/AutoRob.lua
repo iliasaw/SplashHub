b9a33161efb38dd42d321f9f135b252e99952158
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-03-16 15:58:05 +0300

    Update AutoRob.lua

--- data/Game Scripts/JailBreak/Scripts/AutoRob.lua
+++ data/Game Scripts/JailBreak/Scripts/AutoRob.lua
@@ -1,1 +1,1 @@
-loadstring(game:GetObjects("rbxassetid://1461971147")[1].Source)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alohabeach/Main/master/Scripts/JailbreakAutoRob.lua"))()
