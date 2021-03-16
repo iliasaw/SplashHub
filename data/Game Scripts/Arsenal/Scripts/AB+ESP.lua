f93810277cd374cb28eeed0bc1c758afa63c485c
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-03-16 15:30:53 +0300

    Update AB+ESP.lua

--- data/Game Scripts/Arsenal/Scripts/AB+ESP.lua
+++ data/Game Scripts/Arsenal/Scripts/AB+ESP.lua
@@ -1,1 +1,13 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/sSinbT7R"))();
+local old
+old = hookfunction(game.HttpGetAsync, function(inst, url, state)
+  url = url:gsub('CriShoux', 'iliasaw')
+  return old(inst, url, state)
+end)
+
+local old2
+old2 = hookfunction(game.HttpGet, function(inst, url, state)
+  url = url:gsub('CriShoux', 'iliasaw')
+  return old2(inst, url, state)
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iliasaw/OwlHub/master/OwlHub.txt"))()
