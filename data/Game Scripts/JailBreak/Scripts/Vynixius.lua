9d842b34eb5efafb6ee9a5f02d70b6334731d452
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-03-21 19:52:26 +0300

    Update Vynixius.lua

--- data/Game Scripts/JailBreak/Scripts/Vynixius.lua
+++ data/Game Scripts/JailBreak/Scripts/Vynixius.lua
@@ -1,1 +1,11 @@
+local Krnl = KRNL_LOADED and true or false
+
+if Krnl then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Scripts/master/Vynixius%20Jailbreak", true))()
+else
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Splash";
+    Text = "Pls change API in Splash on KRNL.";
+    Duration = 3;
+})
+end
