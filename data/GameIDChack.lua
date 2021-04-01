972ff0f25729e17475a273228342fada22dd5ced
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-04-01 16:26:11 +0300

    Update GameIDChack.lua

--- data/GameIDChack.lua
+++ data/GameIDChack.lua
@@ -4,5 +4,7 @@ elseif game.PlaceId == 698448212 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/notclosure/new-years/main/happ.lua"))()
 elseif game.PlaceId == 606849621 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/iliasaw/SplashHub/main/data/Game%20Scripts/JailBreak/Gui.lua"))()
+elseif game.PlaceId == 2512643572 then
+    loadstring(game:HttpGet(("http://roguefamily.com/script/streleziaBGSbeta/script"),true))()
 else warn("Unknown game!!!")
 end
