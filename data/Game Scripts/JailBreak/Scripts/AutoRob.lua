947ede6485a5466cb4da9dde64e2df5bc16af614
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-04-01 13:51:49 +0300

    Update AutoRob.lua

--- data/Game Scripts/JailBreak/Scripts/AutoRob.lua
+++ data/Game Scripts/JailBreak/Scripts/AutoRob.lua
@@ -1,1 +1,11 @@
+local Krnl = KRNL_LOADED and true or false
+
+if Krnl then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/alohabeach/Main/master/Scripts/JailbreakAutoRob.lua"))()
+else
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Splash";
+    Text = "Pls change API in Splash to KRNL.";
+    Duration = 5;
+})
+end
