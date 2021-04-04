bd14d339738a5c941963f8082dfedb5bf6511087
iliasaw <62304861+iliasaw@users.noreply.github.com>
2021-04-04 20:37:21 +0300

    Update HyHUB_menu.lua

--- data/HyHUB_menu.lua
+++ data/HyHUB_menu.lua
@@ -10,7 +10,7 @@ section1:Button("Game Scripts",function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/iliasaw/SplashHub/main/data/GameIDChack.lua",true))()
 end)
 
-section1:Dropdown("Scripts",{"Ace Of Spadez", "Arsenal", "Blood Moon Tycoon", "Bubble Gum Simulator", "JailBreak", "Lumber Tycoon 2", "Mad Paintball 2", "Project Lazarus", "Polybattle", "RoBeats", "SCP: Roleplay", "Super Power Training Simulator"},function(text)
+section1:Dropdown("Scripts",{"Ace Of Spadez", "Arsenal", "Blood Moon Tycoon", "Bubble Gum Simulator", "JailBreak", "Lumber Tycoon 2", "Mad Paintball 2", "Project Lazarus", "Polybattle", "RoBeats", "SCP: Roleplay", "Super Power Training Simulator", "THE RAKE: Classic Edition"},function(text)
 	if text == "Arsenal" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/iliasaw/SplashHub/main/data/Game%20Scripts/Arsenal/ArsenalMenu.lua",true))()
 	elseif text == "RoBeats" then
@@ -35,6 +35,8 @@ section1:Dropdown("Scripts",{"Ace Of Spadez", "Arsenal", "Blood Moon Tycoon", "B
 		loadstring(game:HttpGet("https://pastebin.com/raw/xjTHrvs2",true))()
 	elseif text == "Ace Of Spadez" then
 		loadstring(game:HttpGet("https://pastebin.com/raw/YffiupxC",true))()
+	elseif text == "THE RAKE: Classic Edition" then
+		loadstring(game:HttpGet("https://pastebin.com/raw/Wft3qUtc", true))()
 	end
 end)
 
